c3fba32bbb9293ecf6af2f3a098bb408c09e1003
anita00001 <sharma.anita00001@gmail.com>
2025-02-05 20:55:19 +0545

    Package, Numpy and Pandas

---
+++ Phase_1/Section_2/Day_001/mypackage/boolean_operation.py
@@ -0,0 +1,14 @@
+def is_even(number):
+    return number % 2 == 0
+
+def is_odd(number):
+    return number % 2 != 0
+
+def is_positive(number):
+    return number > 0
+
+def is_negative(number):
+    return number < 0
+
+def is_zero(number):
+    return number == 0
---
+++ Phase_1/Section_2/Day_001/mypackage/mathematical_operation.py
@@ -0,0 +1,5 @@
+import arithmetic_operation
+import boolean_operation
+
+print(arithmetic_operation.add(5,6))
+print(boolean_operation.is_even(5))
